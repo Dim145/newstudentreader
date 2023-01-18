d1ddbee170803cce811f4de27379825a3b29af10
julienDeveaux <julien.deveaux@hotmail.com>
2023-01-18 11:42:35 +0100

    Show tag id info on screen

--- app/src/main/java/com/example/newstudentclassifier/MainActivity.java
+++ app/src/main/java/com/example/newstudentclassifier/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.newstudentclassifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Vérifiez si l'appareil prend en charge NFC
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "Cet appareil ne prend pas en charge NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "Veuillez activer NFC.", Toast.LENGTH_LONG).show();
+        }
+
+        // Gestion de l'événement de lecture NFC
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Ecoute des événements NFC
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Arrêt de l'écoute des événements NFC
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            // Récupération des données NFC
+            Tag tag = intent.getParcelableExtra("android.nfc.extra.TAG");
+            System.out.println(tag);
+            String message = new String(tag.getId(), StandardCharsets.UTF_8);
+
+
+            // Affichage du message
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(message);
+        }
     }
 }
